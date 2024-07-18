f3c76cf6d09ff9bad02fff51ab22751856ab5391
ZXW <zhouxingwangchina@outlook.com>
2024-07-18 22:06:45 +0800

    导航栏和部分头部栏样式实现

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,3 +1,102 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div>Header</div>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <a href="#"></a>
+      </div>
+
+      <div class="shortcut">
+        <ul>
+          <li><a href="#">首页</a></li>
+          <li><a href="#">居家</a></li>
+          <li><a href="#">美食</a></li>
+          <li><a href="#">服饰</a></li>
+          <li><a href="#">母婴</a></li>
+          <li><a href="#">个护</a></li>
+          <li><a href="#">严选</a></li>
+          <li><a href="#">数码</a></li>
+          <li><a href="#">运动</a></li>
+          <li><a href="#">杂项</a></li>
+        </ul>
+      </div>
+
+      <div class="search">
+        <i class="iconfont icon-sousuokuang"></i>
+        <input type="text" placeholder="搜一搜" />
+      </div>
+      <div class="shopping-cart">
+        <i class="iconfont icon-gouwuchekong"></i>
+      </div>
+    </div>
+  </header>
 </template>
+
+<style scoped lang="scss">
+header {
+  height: 132px;
+  background-color: #fff;
+}
+
+// logo
+.logo {
+  float: left;
+}
+
+.logo a {
+  display: block;
+  width: 200px;
+  height: 132px;
+  background: url("@/assets/images/logo.png") no-repeat center 18px / contain;
+}
+
+//导航栏
+ul {
+  float: left;
+  margin-left: 40px;
+  font-size: 16px;
+}
+
+ul li {
+  float: left;
+  width: 78px;
+  height: 132px;
+  line-height: 132px;
+  font-size: 16px;
+  color: #121426;
+}
+
+// 搜索框和购物车
+.search {
+  float: left;
+  width: 170px;
+  height: 32px;
+  margin: 50px 0;
+  border-bottom: 1px solid #e7e7e7;
+}
+
+.search i {
+  float: left;
+  font-size: 20px;
+}
+
+input {
+  float: left;
+  width: 140px;
+  margin: 8px 0 0 5px;
+  color: #666;
+}
+
+.shopping-cart {
+  float: right;
+  width: 50px;
+  height: 132px;
+  line-height: 132px;
+  text-align: center;
+}
+
+.shopping-cart i {
+  font-size: 22px;
+}
+</style>
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,3 +1,48 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div>导航栏</div>
+  <nav class="nav">
+    <div class="container">
+      <ul>
+        <li><a href="#" @click="$router.push('/login')">请先登录</a></li>
+        <li></li>
+        <li><a href="#">帮助中心</a></li>
+        <li></li>
+        <li><a href="#">关于我们</a></li>
+      </ul>
+    </div>
+  </nav>
 </template>
+
+<style scoped lang="scss">
+  .nav{
+    height: 52px;
+    background-color: #333333;
+  }
+
+  ul{
+    float: right
+  }
+
+  li{
+    float: left;
+  }
+
+  li:nth-child(even){
+    height: 16px;
+    margin: 18px 16px;
+    border-left: 1px solid #666;
+  }
+
+  a{
+    height: 52px;
+    line-height: 52px;
+    font-size: 14px;
+    color: #cdcdcd;
+  }
+
+  a:hover{
+    color: $xtxColor;
+  }
+</style>
